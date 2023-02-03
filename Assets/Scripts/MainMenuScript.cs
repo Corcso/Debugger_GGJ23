22a29a5e3b4f61e7cf91e0907f3eaafaeff52981
corcso <cormacsomerville@outlook.com>
2023-02-03 22:16:22 +0000

    Added: Functions for Title Screen

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Function to Quit the game
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("Play Game Presssed");
+    }
+
+    public void ShowControls()
+    {
+        Debug.Log("Show Controls Presssed");
+    }
+}
