c643ff8a5bdf055c6cb18d4a0ae1b61cc7339c03
Voaly <55096594+Voaly@users.noreply.github.com>
2023-02-03 22:10:29 +0000

    Commit: Camera movement with the player
    Massive brain

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -18,17 +18,24 @@ public class characterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
         //playerTransform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * speed;
-        playerRigidbody.AddForce(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * speed);
+        playerRigidbody.AddForce(new Vector2(horizontalAxis, verticalAxis) * Time.deltaTime * speed);
 
         Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        //Vector3 lookAt = mouseScreenPosition;
 
         float AngleRad = Mathf.Atan2(mouseScreenPosition.y - this.transform.position.y, mouseScreenPosition.x - this.transform.position.x);
 
         float AngleDeg = (180 / Mathf.PI) * AngleRad;
 
         this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+
+
+        //assuming we only using the single camera:
+        var camera = Camera.main;
+
+        camera.transform.position = new Vector3(this.transform.position.x,this.transform.position.y,-10);
     }
 }
