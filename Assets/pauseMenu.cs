d8d1a786bb11532c36f8d0d87b29ae32e2efef8e
Voaly <55096594+Voaly@users.noreply.github.com>
2023-02-04 21:18:26 +0000

    Update: pause is workingggg

--- Assets/pauseMenu.cs
+++ Assets/pauseMenu.cs
@@ -3,27 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class pauseMenu : MonoBehaviour
 {
+    public gameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<gameManager>(); 
     }
 
     public GameObject pauseMenuObject;
+ 
     float previousEsc = 0;
     bool isPauseOpen = false;
     // Update is called once per frame
     void Update()
     {
+       
         
         
         if (Input.GetAxis("Cancel") == 1 && previousEsc != 1)
         {
             isPauseOpen = !isPauseOpen;
             pauseMenuObject.SetActive(isPauseOpen);
-            Debug.Log("test");
+            
+            if (isPauseOpen)
+            {
+                gameManager.currentGameState = gameManager.gameState.paused;
+            }
+            else
+            {
+                gameManager.currentGameState = gameManager.gameState.play
+            }
         }
         previousEsc = Input.GetAxis("Cancel");
     }
