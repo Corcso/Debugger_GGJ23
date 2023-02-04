54e7708af9459b277a97fab9b82cbf3b26000235
corcso <cormacsomerville@outlook.com>
2023-02-04 17:26:27 +0000

    Added: Bug Spawn Script

---
+++ Assets/enemySpawnScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class enemySpawnScript : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public GameObject bugPrefab;
+    public float bugInterval;
+    float timeSinceLastSpawn = 0;
+    Vector2 spawnMin = new Vector2(10, 6);
+    Vector2 spawnMax = new Vector2(40, 36);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.realtimeSinceStartup >= timeSinceLastSpawn + bugInterval) {
+            timeSinceLastSpawn = Time.realtimeSinceStartup;
+
+            float spawnX = Random.Range(spawnMin.x + playerTransform.position.x, spawnMax.x + playerTransform.position.x);
+            float spawnY = Random.Range(spawnMin.y + playerTransform.position.y, spawnMax.y + playerTransform.position.y);
+
+            if (Random.Range(0, 2) == 0) {
+                spawnX = Random.Range(-spawnMin.x + playerTransform.position.x, -spawnMax.x + playerTransform.position.x);
+            }
+            if (Random.Range(0, 2) == 0) {
+                spawnY = Random.Range(-spawnMin.y + playerTransform.position.y, -spawnMax.y + playerTransform.position.y);
+            }
+            if (Random.Range(0, 2) == 0){
+                spawnX = Random.Range(-spawnMax.x + playerTransform.position.x, spawnMax.x + playerTransform.position.x);
+            }
+            else {
+                spawnY = Random.Range(-spawnMax.y + playerTransform.position.y, spawnMax.y + playerTransform.position.y);
+            }
+
+            spawnX = Mathf.Clamp(spawnX, -60, 60);
+            spawnY = Mathf.Clamp(spawnY, -60, 60);
+
+            Instantiate(bugPrefab, new Vector3(spawnX, spawnY, 0), Quaternion.Euler(0, 0, 0));
+        }
+    }
+}
