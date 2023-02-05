a7c499a90d68b2a18756afbdabc983bedb1e748d
corcso <cormacsomerville@outlook.com>
2023-02-05 13:34:19 +0000

    Added: Sound to the pre game screen

--- Assets/LoginScript.cs
+++ Assets/LoginScript.cs
@@ -12,6 +12,9 @@ public class LoginScript : MonoBehaviour
     public TextMeshProUGUI usernameText;
     public TextMeshProUGUI passwordText;
     public Image Login;
+
+    public AudioSource keyPressSound;
+    public AudioSource clickSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,77 +29,92 @@ public class LoginScript : MonoBehaviour
             timeSinceLastTstep = Time.realtimeSinceStartup;
             if (currentTextFrame == 0)
             {
+                keyPressSound.Play();
                 usernameText.text = "R";
                 currentTextFrame++;
 
             }
             else if (currentTextFrame == 1)
             {
+                keyPressSound.Play();
                 usernameText.text = "RO";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 2)
             {
+                keyPressSound.Play();
                 usernameText.text = "ROO";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 3)
             {
+                keyPressSound.Play();
                 usernameText.text = "ROOT";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 4)
             {
+                keyPressSound.Play();
                 passwordText.text = "*";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 5)
             {
+                keyPressSound.Play();
                 passwordText.text = "**";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 6)
             {
+                keyPressSound.Play();
                 passwordText.text = "***";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 7)
             {
+                keyPressSound.Play();
                 passwordText.text = "****";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 8)
             {
+                keyPressSound.Play();
                 passwordText.text = "*****";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 9)
             {
+                keyPressSound.Play();
                 passwordText.text = "******";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 10)
             {
+                keyPressSound.Play();
                 passwordText.text = "*******";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 11)
             {
+                keyPressSound.Play();
                 passwordText.text = "********";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 12)
             {
+                keyPressSound.Play();
                 passwordText.text = "*********";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 11)
             {
+                keyPressSound.Play();
                 passwordText.text = "**********";
                 currentTextFrame++;
             }
             else if (currentTextFrame == 13)
             {
+                clickSound.Play();
                 Login.color = new Color(0.6f, 0.6f, 0.6f);
                 currentTextFrame++;
             }
